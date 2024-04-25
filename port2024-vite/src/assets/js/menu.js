d5412444d606aae98dee50ee6e35a1241e0c417a
JoSungHyeon <jsh950618@gmail.com>
2024-04-25 14:20:46 +0900

    header commit

--- port2024-vite/src/assets/js/menu.js
+++ port2024-vite/src/assets/js/menu.js
@@ -1,3 +1,15 @@
 export function menu() {
-    console.log(menu)
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header_nav");
+    if(headerToggle) {
+        headerToggle.addEventListener("click", ()=>{
+            headerNav.classList.toggle("show");
+    
+    
+    
+            headerToggle.getAttribute("aria-expended") === "true"
+            ? headerToggle.setAttribute("aria-expended", "false")
+            : headerToggle.setAttribute("aria-expended", "true")
+        });
+    }
 }
