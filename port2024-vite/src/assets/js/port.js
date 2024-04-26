4d68ad3fdc10fa8a8a6c0950375e1da6e3255d64
JoSungHyeon <jsh950618@gmail.com>
2024-04-26 14:26:02 +0900

    port commit

--- port2024-vite/src/assets/js/port.js
+++ port2024-vite/src/assets/js/port.js
@@ -1,3 +1,23 @@
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+
 export function port() {
-    console.log(port)
+    gsap.registerPlugin(ScrollTrigger);
+
+    const horSection = gsap.utils.toArray(".port_item");
+
+    gsap.to(horSection, {
+        xPercent: -100 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#port",
+            start: "top 60px",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        }
+    });
 }
