783fd8feac1a98336fc4fc311c90e5ca4fe98bce
JoSungHyeon <jsh950618@gmail.com>
2024-04-30 15:01:06 +0900

    react setting commit

--- port2024-react/src/App.js
+++ port2024-react/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeView from './views/HomeView';
+import AboutView from './views/AboutView';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<HomeView />} />
+                <Route path='/about' element={<AboutView />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ port2024-react/src/views/AboutView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutView = () => {
+  return (
+    <div>AboutView</div>
+  )
+}
+
+export default AboutView
