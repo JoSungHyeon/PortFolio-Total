3942a6635eab1f8b64e95bcc206211eedddf18cd
JoSungHyoen <jsh950618@gmail.com>
2024-05-06 17:45:07 +0900

    header commit

--- port2024-react/src/components/Header.jsx
+++ port2024-react/src/components/Header.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from "react"
+
+const headerNav = [
+    {
+      title: "intro",
+      url: "#intro"
+    },
+    {
+      title: "skill",
+      url: "#skill"
+    },
+    {
+      title: "site",
+      url: "#site"
+    },
+    {
+      title: "portfolio",
+      url: "#port"
+    },
+    {
+      title: "contact",
+      url: "#contact"
+    }
+];
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  }
+
   return (
-    <div>Header</div>
+    <header id="header" role="banner">
+        <div className="header_inner">
+            <div className="header_logo">
+                <h1>
+                    <a href="/">SH<em>vite</em></a>
+                </h1>
+            </div>
+            <nav
+              className={`header_nav ${show ? "show" : ""}`}
+              role="navigation" aria-label="메인메뉴"
+            >
+                <ul>
+                  {headerNav.map((nav, key) => (
+                      <li key={key}>
+                        <a href={nav.url}>{nav.title}</a>
+                      </li>
+                  ))}
+                </ul>
+            </nav>
+            <div
+                className="header_nav_mobile"
+                id="headerToggle"
+                aria-controls="primary-menu"
+                aria-expanded={show ? "true" : "false"}
+                role="button"
+                tabIndex="0"
+                onClick={toggleMenu}
+            >
+            <span></span>
+        </div>
+        </div>
+    </header>
   )
 }
 
--- port2024-vite/src/assets/js/menu.js
+++ port2024-vite/src/assets/js/menu.js
@@ -5,8 +5,6 @@ export function menu() {
         headerToggle.addEventListener("click", ()=>{
             headerNav.classList.toggle("show");
     
-    
-    
             headerToggle.getAttribute("aria-expended") === "true"
             ? headerToggle.setAttribute("aria-expended", "false")
             : headerToggle.setAttribute("aria-expended", "true")
