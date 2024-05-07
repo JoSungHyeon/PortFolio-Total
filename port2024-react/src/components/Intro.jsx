bcb2268b2aaa29fbe48703d5d08ae6110cf17629
JoSungHyeon <jsh950618@gmail.com>
2024-05-07 15:05:42 +0900

    site commit

--- port2024-react/src/components/Intro.jsx
+++ port2024-react/src/components/Intro.jsx
@@ -9,40 +9,40 @@ const introText = {
 const Intro = () => {
   return (
     <section id="intro">
-            <div className="intro_inner">
-                <h2 className="intro_title">
-                    {introText.title}
-                </h2>
-                <div className="intro_lines" aria-hidden="true">
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
+        <div className="intro_inner">
+            <h2 className="intro_title">
+                {introText.title}
+            </h2>
+            <div className="intro_lines" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+            <div className="intro_text">
+                <div className="text">
+                    <div>{introText.desc[0]}</div>
+                    <div>{introText.desc[1]}</div>
+                    <div>{introText.desc[2]}</div>
                 </div>
-                <div className="intro_text">
-                    <div className="text">
-                        <div>{introText.desc[0]}</div>
-                        <div>{introText.desc[1]}</div>
-                        <div>{introText.desc[2]}</div>
-                    </div>
-                    <div className="img">
-                        <img src={about} alt="my img" />
-                    </div>
-                </div>
-                <div className="intro_lines bottom" aria-hidden="true">
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
+                <div className="img">
+                    <img src={about} alt="my img" />
                 </div>
             </div>
-        </section>
+            <div className="intro_lines bottom" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+        </div>
+    </section>
   )
 }
 
--- port2024-react/src/components/Site.jsx
+++ port2024-react/src/components/Site.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import React from 'react';
+
+const siteText = [
+    {
+        text: ["make", "site compliant with", "webstandard"],
+        title: "Create a site using Vite",
+        code: "https://github.com/JoSungHyeon/PortFolio-Total",
+        view: "/",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : html5/css3, CSS Variable, Vite"
+        ]
+    },
+    {
+        text: ["make", "site compliant with", "webstandard"],
+        title: "Create a site using React",
+        code: "https://github.com/JoSungHyeon/PortFolio-Total",
+        view: "/",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : html5/css3, CSS Variable, React"
+        ]
+    },
+    {
+        text: ["make", "site compliant with", "webstandard"],
+        title: "Create a site using Vue",
+        code: "https://github.com/JoSungHyeon/PortFolio-Total",
+        view: "/",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : html5/css3, CSS Variable, Vue"
+        ]
+    },
+    {
+        text: ["make", "site compliant with", "webstandard"],
+        title: "Create a site using Next",
+        code: "https://github.com/JoSungHyeon/PortFolio-Total",
+        view: "/",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : html5/css3, CSS Variable, Next"
+        ]
+    },
+]
 
 const Site = () => {
   return (
-    <div>Site</div>
+    <section id="site">
+        <div className="site_inner">
+            <h2 className="site_title">
+            Site Coding <em>my site</em>
+        </h2>
+        <div className="site_wrap">
+            {siteText.map((site, key) => (
+                <article className={`site_item s${key+1}`} key={key}>
+                    <span className="num">{key+1}</span>
+                    <div className="text">
+                        <div>{site.text[0]}</div>
+                        <div>{site.text[1]}</div>
+                        <div>{site.text[2]}</div>
+                    </div>
+                    <h3 className="title">
+                        {site.title}
+                    </h3>
+                    <div className="btn">
+                      <a href={site.code}>code</a>
+                      <a href={site.view}>view</a>
+                    </div>
+                    <div className="info">
+                        <span>{site.info[0]}</span>
+                        <span>{site.info[1]}</span>
+                        <span>{site.info[2]}</span>
+                    </div>
+                </article>
+            ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
